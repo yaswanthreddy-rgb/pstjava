1c35d5538f1a0330aac8f87f612593aa0f237c52
Gontu Yaswanth <yaswanthreddygontu@gmail.com>
2026-08-22 11:20:18 +0530

    Implement matrix transposition method

---
+++ Transpose Matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n= matrix.length;
+        int m=  matrix[0].length;
+        int [][]result=new int[m][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                result[j][i]=matrix[i][j];
+                
+            }
+        }
+        return result;
+    }
+}
